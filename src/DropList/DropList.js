130ba71a0b6899b21aadf48d443db2d467d62cbd
hontar27 <59845299+hontar27@users.noreply.github.com>
2020-04-21 15:52:01 +0300

    works

--- src/DropList/DropList.js
+++ src/DropList/DropList.js
@@ -1,26 +1,13 @@
 import React from 'react';
-// import './DropList.css';
 
-class DropList extends React.Component {
-
-    render() {
-        const listItems = this.props.items.map((item,index)=>(
-            <div onClick={() => this.props.toggleComplete(index)}>
-                <li  style={{
-                    textDecoration: this.props.items.complete ? "line-through" : ""
-                }}
-                     key={item.id}>
-                    {item.newItem}
-                    <button onClick={() =>this.props.deleteItem(index)}>Deleted
-                    </button>
-                </li>
-            </div>
-        ));
-        return (
-            <>
-                <ul>{listItems}</ul>
-            </>
-        )
-    }
-}
-export default DropList
+export default props => (
+    <div
+        style={{
+            textDecoration: props.item.complete ? "line-through" : ""
+        }}
+        onClick={props.toggleComplete}
+    >
+        <li>{props.item.newItem}</li>
+        <button onClick={props.deleteItem}>delete</button>
+    </div>
+);
